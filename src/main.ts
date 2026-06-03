4c42d2b33f61d18500bf78e29f61c054e6b43ea0
MorningstarDev <matdourodriguez@gmail.com>
2026-06-03 20:15:02 -0300

    Swagger initial setup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,24 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+  .setTitle('Plataforma Cine API')
+  .setDescription('API para la red social de cine')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+
+const document = SwaggerModule.createDocument(
+  app,
+  config,
+);
+
+SwaggerModule.setup(
+  'api',
+  app,
+  document,
+);
 
   await app.listen(process.env.PORT ?? 3000);
 }
