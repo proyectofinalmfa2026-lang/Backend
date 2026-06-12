e31f5ec81736de2b356516a77fe352b70133494f
Alejandro-Mirena <alejandromirena.13@gmail.com>
2026-06-12 17:20:15 -0500

    fix(cors): configure frontend URL for production

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true,
+    origin: [process.env.FRONTEND_URL, 'http://localhost:3000'],
     credentials: true,
   });
   app.useGlobalPipes(
